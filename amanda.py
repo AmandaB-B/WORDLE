ce97bcaedf068488982f4c0a6e71a5f246725db5
Amanda Beebe <abeebe001@student.franciscan.edu>
2025-05-02 13:18:08 -0400

    s

--- amanda.py
+++ amanda.py
@@ -105,6 +105,10 @@ frame2.pack(pady=5)
 frame2.rowconfigure([0], minsize = 5)
 frame2.columnconfigure([0,1,2,3,4,5,6,7,8,9], minsize = 5)
 
+
+
+
+
 label_grid = [
     [label1, label2, label3, label4, label5],
     [label6, label7, label8, label9, label10],
